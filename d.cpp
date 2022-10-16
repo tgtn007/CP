e0579f399f7034b8d7611066fda96ff434e92b07
Dip31032001 <92220594+Dip31032001@users.noreply.github.com>
2022-10-16 18:06:20 +0530

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	cout<<"Happy to see you using this scientific calculator"<<endl;
+	cout<<"Choose what operation do you want"<<endl;
+	cout<<"1.Addition 
+	2.sub 
+	3.multi 
+	4.div";
+}
